e1ee20c7936024345852496dfe5b4a3bc2ad8e70
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-11-13 12:10:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,10 @@ setup(
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Tanmay Agarwal',                   # Type in your name
   author_email = 'tagarwal_be18@thapar.edu',      # Type in your E-Mail
-  url = 'https://github.com/Tanmay3610',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  url = 'https://github.com/Tanmay3610/TOPSIS_Package',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Tanmay3610/TOPSIS_Package/archive/0.1.tar.gz',    # I explain this later on
+  keywords = ['TOPSIS', 'DATA SCIENCE', 'DATA ANALYSIS'],   # Keywords that define your package best
+  install_requires=['pandas', 'numpy', 'sys', 'math'],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
